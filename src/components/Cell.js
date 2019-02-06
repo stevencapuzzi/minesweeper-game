ed9743fc5772cba9a09c440de007d3d813c69a57
Steven Capuzzi <stevenczz@gmail.com>
2019-02-06 11:07:39 -0500

    css and prop types call in the Cell componenet

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,40 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Cell extends Component {
-    getValue() {
-        const {value} = this.props;
-        if (!value.isRevealed){
-            return this.props.value.isFlagged ? "🚩" : null;
-        }
-        if (value.isMine) {
-            return "💣";
-        }
-        if (value.neighbour === 0) {
-          return null;
-        }
-        return value.neighbour;
-        }
+export default class Cell extends React.Component {
+  getValue() {
+    const { value } = this.props;
 
-    render() {
-        const {value, onClick, cMenu} = this.props;    
-        return (
-          <div
-            onClick={this.props.onClick}
-            onContextMenu={this.props.cMenu}
-          >
-          {this.getValue()}
-          </div>
-        );
-      }
+    if (!value.isRevealed) {
+      return this.props.value.isFlagged ? "🚩" : null;
+    }
+    if (value.isMine) {
+      return "💣";
+    }
+    if (value.neighbour === 0) {
+      return null;
+    }
+    return value.neighbour;
+  }
+
+  render() {
+    const { value, onClick, cMenu } = this.props;
+    let className =
+      "cell" +
+      (value.isRevealed ? "" : " hidden") +
+      (value.isMine ? " is-mine" : "") +
+      (value.isFlagged ? " is-flag" : "");
+
+    return (
+      <div
+        onClick={onClick}
+        className={className}
+        onContextMenu={cMenu}
+      >
+        {this.getValue()}
+      </div>
+    );
+  }
 }
 
 const cellItemShape = {
     isRevealed: PropTypes.bool,
     isMine: PropTypes.bool,
     isFlagged: PropTypes.bool
-  }
-  Cell.propTypes = {
-    value: PropTypes.objectOf(PropTypes.shape(cellItemShape)),
-    onClick: PropTypes.func,
-    cMenu: PropTypes.func
-  }
+}
+
+Cell.propTypes = {
+  value: PropTypes.objectOf(PropTypes.shape(cellItemShape)),
+  onClick: PropTypes.func,
+  cMenu: PropTypes.func
+}
